53c3b5779415094052231903379c704ddf64d06d
antoniovuono <antoniovuono@brq.com>
2023-08-31 22:37:30 -0300

    bugfix: Project finalized from IOS: Step counter, distance walking and flights integrated

--- App.tsx
+++ App.tsx
@@ -2,29 +2,97 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import Value from "./src/components/Value";
 import { RingProgress } from "./src/components/RingProgress";
-import AppleHealthKit, { HealthKitPermissions } from "react-native-health";
-import { useEffect } from "react";
+import AppleHealthKit, {
+  HealthInputOptions,
+  HealthKitPermissions,
+  HealthUnit,
+} from "react-native-health";
+import { useEffect, useState } from "react";
+
+const STEPS_GOAL = 10_000;
 
 export default function App() {
+  const [hasPermissions, setHasPermissions] = useState(false);
+  const [setps, setSteps] = useState(0);
+  const [flights, setFlights] = useState(0);
+  const [distance, setDistance] = useState(0);
+
   const permissions: HealthKitPermissions = {
     permissions: {
-      read: [AppleHealthKit.Constants.Permissions.Steps],
+      read: [
+        AppleHealthKit.Constants.Permissions.Steps,
+        AppleHealthKit.Constants.Permissions.FlightsClimbed,
+        AppleHealthKit.Constants.Permissions.DistanceWalkingRunning,
+      ],
       write: [],
     },
   };
 
   useEffect(() => {
-    AppleHealthKit.isAvailable(() => {});
-  }, []);
+    AppleHealthKit.initHealthKit(permissions, (err) => {
+      if (err) {
+        console.error("Error getting permissions");
+        return;
+      }
+
+      setHasPermissions(true);
+    });
+  });
+
+  useEffect(() => {
+    if (!hasPermissions) {
+      return;
+    }
+
+    // Healthkit Options
+    const options: HealthInputOptions = {
+      date: new Date().toISOString(),
+      includeManuallyAdded: false,
+    };
+
+    // Get Setps from IOS:
+    AppleHealthKit.getStepCount(options, (err, results) => {
+      if (err) {
+        console.log("Error getting steps");
+        return;
+      }
+
+      setSteps(results.value);
+    });
+
+    // Get Flights climbed from IOS:
+    AppleHealthKit.getFlightsClimbed(options, (err, results) => {
+      if (err) {
+        console.log("Error getting steps");
+        return;
+      }
+
+      setFlights(results.value);
+    });
+
+    // Get Distance from IOS:
+    AppleHealthKit.getDistanceWalkingRunning(options, (err, results) => {
+      if (err) {
+        console.log("Error getting steps");
+        return;
+      }
+
+      setDistance(results.value);
+    });
+  }, [hasPermissions]);
 
   return (
     <View style={styles.container}>
-      <RingProgress radius={150} strokeWidth={50} progress={0.2} />
+      <RingProgress
+        radius={150}
+        strokeWidth={50}
+        progress={setps / STEPS_GOAL}
+      />
 
       <View style={styles.values}>
-        <Value label="Steps" value="1231" />
-        <Value label="Distance" value="0,75 km" />
-        <Value label="Flights Climbed" value="Steps" />
+        <Value label="Steps" value={setps.toString()} />
+        <Value label="Distance" value={`${(distance / 1000).toFixed(2)} km`} />
+        <Value label="Flights Climbed" value={flights.toString()} />
       </View>
 
       <StatusBar style="auto" />
